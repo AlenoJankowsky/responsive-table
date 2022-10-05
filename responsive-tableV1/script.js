2b996da64da8f0d11d7e8cbf0677307f27dea02e
Aleno <aleno.jankowsky@5minds.de>
2022-10-05 08:19:05 +0200

    Change style of responsive table

--- responsive-tableV1/script.js
+++ responsive-tableV1/script.js
@@ -2,29 +2,29 @@ let oldWindowWidth = null;
 let oldWindowHeight = null;
 function changeTableSize() {
   let tableWidth = document.getElementById('body__table-id').offsetWidth;
+  let tableHeight = document.getElementById('body__table-id').offsetHeight;
   let widthSizeChangesPositively = oldWindowWidth < window.outerWidth;
   let widthSizeChangesNegatively = oldWindowWidth > window.outerWidth;
   let heightSizeChangesPositively = oldWindowHeight < window.outerHeight;
   let heightSizeChangesNegatively = oldWindowHeight > window.outerHeight;
   if (widthSizeChangesPositively && oldWindowWidth != null) {
-    let tableWidth = document.getElementById('body__table-id').offsetWidth;
-    let computedNewWidth = tableWidth + 100;
+    let computedNewWidth = tableWidth + 3;
     let computedNewWidthToString = computedNewWidth.toString();
-    let joinedStringForWidth = computedNewWidthToString + "px"
+    let joinedStringForWidth = computedNewWidthToString + "px";
     document.getElementById('body__table-id').style.width = joinedStringForWidth;
-
-    return;
   }
   else if (widthSizeChangesNegatively && oldWindowWidth != null) {
-    let tableWidth = document.getElementById('body__table-id').offsetWidth;
-    let computedNewWidth = tableWidth - 100;
+    let computedNewWidth = tableWidth - 3;
     let computedNewWidthToString = computedNewWidth.toString();
-    let joinedStringForWidth = computedNewWidthToString + "px"
+    let joinedStringForWidth = computedNewWidthToString + "px";
     document.getElementById('body__table-id').style.width = joinedStringForWidth;
-
-    return;
   }
-
+  else if (heightSizeChangesPositively) {
+    let computedNewHeight = tableHeight -3;
+    let computedNewHeightToString = computedNewHeight.toString();
+    let joinedStringForHeight = computedNewHeightToString + "px";
+    document.getElementById('body__table-id').style.height = joinedStringForHeight;
+  }
 
   oldWindowWidth = window.outerWidth;
   oldWindowHeight = window.outerHeight;

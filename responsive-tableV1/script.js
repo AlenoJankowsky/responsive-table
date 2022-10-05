26257cb592680cc18a8b716cad7cb035442a9b66
Aleno <aleno.jankowsky@5minds.de>
2022-10-05 10:27:08 +0200

    Fix styling issues

--- responsive-tableV1/script.js
+++ responsive-tableV1/script.js
@@ -1,34 +1,50 @@
 let oldWindowWidth = null;
 let oldWindowHeight = null;
+
 function changeTableSize() {
   let tableWidth = document.getElementById('body__table-id').offsetWidth;
+  
   let tableHeight = document.getElementById('body__table-id').offsetHeight;
+
   let widthSizeChangesPositively = oldWindowWidth < window.outerWidth;
   let widthSizeChangesNegatively = oldWindowWidth > window.outerWidth;
   let heightSizeChangesPositively = oldWindowHeight < window.outerHeight;
   let heightSizeChangesNegatively = oldWindowHeight > window.outerHeight;
+
   if (widthSizeChangesPositively && oldWindowWidth != null) {
-    let computedNewWidth = tableWidth + 3;
+    let computedNewWidth = tableWidth + 4;
+
     let computedNewWidthToString = computedNewWidth.toString();
+
     let joinedStringForWidth = computedNewWidthToString + "px";
+
     document.getElementById('body__table-id').style.width = joinedStringForWidth;
   }
   else if (widthSizeChangesNegatively && oldWindowWidth != null) {
-    let computedNewWidth = tableWidth - 3;
+    let computedNewWidth = tableWidth - 4;
+
     let computedNewWidthToString = computedNewWidth.toString();
+
     let joinedStringForWidth = computedNewWidthToString + "px";
+
     document.getElementById('body__table-id').style.width = joinedStringForWidth;
   }
   else if (heightSizeChangesPositively) {
-    let computedNewHeight = tableHeight + 3;
+    let computedNewHeight = tableHeight + 4;
+
     let computedNewHeightToString = computedNewHeight.toString();
+
     let joinedStringForHeight = computedNewHeightToString + "px";
+
     document.getElementById('body__table-id').style.height = joinedStringForHeight;
   }
   else if (heightSizeChangesNegatively) {
-    let computedNewHeight = tableHeight - 3;
+    let computedNewHeight = tableHeight - 4;
+
     let computedNewHeightToString = computedNewHeight.toString();
+
     let joinedStringForHeight = computedNewHeightToString + "px";
+
     document.getElementById('body__table-id').style.height = joinedStringForHeight;
   }
 
